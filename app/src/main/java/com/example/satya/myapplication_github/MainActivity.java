be67b9405cbc1577745cfc643d871d37998d0d70
satya <satyapriyapanda1993@gmail.com>
2016-11-13 22:20:07 +0530

    added a method of adding

--- app/src/main/java/com/example/satya/myapplication_github/MainActivity.java
+++ app/src/main/java/com/example/satya/myapplication_github/MainActivity.java
@@ -10,4 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void add()
+    {
+        int i=0, j=0,sum;
+        sum =i+j;
+
+    }
 }
